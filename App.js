4b85d2e555831c5cf755e8a33a73f82e1018604c
lafkpages <mail@lafkpages.tech>
2023-04-18 20:04:02 +0200

    Implement screens

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 const bgPrimary = "#1360dd";
 const fgPrimary = "#ffffff";
 
-export default function App() {
+function Home({ navigation }) {
   return (
     <SafeAreaView style={styles.body}>
       <Text style={styles.h1}>GymBot AI</Text>
       <StatusBar style='auto' />
     </SafeAreaView>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
