35ac2e1efee4b4457eae21f8366ebe73117944fc
lafkpages <mail@lafkpages.tech>
2023-04-18 20:20:12 +0200

    Center home screen

--- styles.js
+++ styles.js
@@ -8,6 +8,9 @@ export default StyleSheet.create({
     backgroundColor: bgPrimary,
     width: "100%",
     height: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   h1: {
     fontSize: 32,
