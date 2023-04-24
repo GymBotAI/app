48da87d26c4b8120e622cd48ff30dcf1a01b9922
lafkpages <mail@lafkpages.tech>
2023-04-24 19:26:50 +0200

    Add MessageInputContainer component

---
+++ components/MessageInputContainer.js
@@ -0,0 +1,22 @@
+import { TextInput, View } from "react-native";
+
+import styles from "../styles";
+
+export default function messageInputContainer({
+  placeholder,
+  returnKeyType,
+  value,
+}) {
+  return (
+    <View style={styles.messageInputContainer}>
+      <TextInput
+        placeholder={placeholder || "Talk to GymBot AI!"}
+        multiline={true}
+        inputMode='text'
+        returnKeyType={returnKeyType || "send"}
+        style={styles.input}
+        value={value || ""}
+      />
+    </View>
+  );
+}
