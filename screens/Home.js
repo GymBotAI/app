0bb8ce68420a488ca8fec2e14747aed9a74dce73
lafkpages <mail@lafkpages.tech>
2023-04-29 11:01:16 +0200

    Demo API usage

--- screens/Home.js
+++ screens/Home.js
@@ -5,11 +5,21 @@ import ChatInput from "../components/ChatInput";
 
 import styles from "../styles";
 
+import { useGymBotAI } from "../api";
+
 export default function HomeScreen({ navigation }) {
+  const [messages, askGymBotAI] = useGymBotAI();
+
   return (
     <SafeAreaView style={styles.body}>
       <Text style={styles.h1}>GymBot AI</Text>
-      <ChatInput onSubmit={(text) => console.log(text)} />
+      <ChatInput
+        onSubmit={(text) =>
+          askGymBotAI("user", text).then(() => {
+            console.log(messages);
+          })
+        }
+      />
       <StatusBar style='auto' />
     </SafeAreaView>
   );
