8dba0ebc27320a85d30d8e757489f95bb670365a
lafkpages <mail@lafkpages.tech>
2023-05-12 10:46:44 +0200

    Rename styles consts, prettify

--- styles.js
+++ styles.js
@@ -1,11 +1,11 @@
 import { StyleSheet } from "react-native";
 
 const bgPrimary = "#4DBCC1";
-const bgMenu = '#408589';
+const bgMenu = "#408589";
 const borderDefault = "#FFFFFF";
-const WHITE = '#FFFFFF';
-const FONTSIZE = 16;
-const FONT = 'Georgia';
+const white = "#FFFFFF";
+const fontSize = 16;
+const font = "Georgia";
 
 export default StyleSheet.create({
   body: {
@@ -18,20 +18,20 @@ export default StyleSheet.create({
   },
   h1: {
     fontSize: 32,
-    color: WHITE,
-    textAlign: 'center',
+    color: white,
+    textAlign: "center",
     backgroundColor: bgMenu,
     width: "100%",
     height: "6.5%",
-    paddingTop: '0.5%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    fontFamily: 'Impact',
-    letterSpacing: '.5px',
-    wordSpacing: '1px',
-    position: 'absolute',
-    top: 0
+    paddingTop: "0.5%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    fontFamily: "Impact",
+    letterSpacing: ".5px",
+    wordSpacing: "1px",
+    position: "absolute",
+    top: 0,
   },
   input: {
     width: "100%",
@@ -41,28 +41,28 @@ export default StyleSheet.create({
     color: "black",
     backgroundColor: bgPrimary,
     padding: 10,
-    width: '95%',
-    justifyContent: 'center',
-    fontFamily: FONT,
-    fontSize: FONTSIZE
+    width: "95%",
+    justifyContent: "center",
+    fontFamily: font,
+    fontSize: fontSize,
   },
   ChatInput: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%'
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
   },
   ChatMessages: {
     padding: 10,
     display: "flex",
     flex: 1,
     gap: 10,
-    flexGrow: '1',
-    justifyContent: 'flex-end',
+    flexGrow: "1",
+    justifyContent: "flex-end",
   },
   ChatMessageText: {
-    color: WHITE,
-    fontSize: FONTSIZE,
-    fontFamily: FONT
+    color: white,
+    fontSize: fontSize,
+    fontFamily: font,
   },
   ChatMessageIcon: {
     width: 32,
@@ -77,6 +77,6 @@ export default StyleSheet.create({
   Chat: {
     padding: 24,
     flex: 1,
-    justifyContent: 'space-around',
+    justifyContent: "space-around",
   },
 });
