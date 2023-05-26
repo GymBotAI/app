29f5802e89fcccaae88266f3841be7b90aaf951c
ShiGame45 <shiven.sheth@gmail.com>
2023-05-26 09:09:11 +0200

    Added custom font to header

--- src/components/ScreenHeader.js
+++ src/components/ScreenHeader.js
@@ -1,15 +1,38 @@
 import { Text } from "react-native";
+import * as Font from 'expo-font';
+import React, { useState, useEffect } from 'react';
+
+
 
 export default function ScreenHeader({ text }) {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFont() {
+      await Font.loadAsync({
+      'custom-font': require('../../assets/fonts/impwrench.ttf'),
+    });
+    
+      setFontLoaded(true);
+    }
+    
+    loadFont();
+    }, []);
+    
+    if (!fontLoaded) {
+    return <Text>Loading...</Text>;
+    }
+
   return (
     <Text
       style={{
-        fontSize: 32,
+        fontSize: 56,
         paddingTop: 40,
         paddingBottom: 5,
         color: "black",
-        fontFamily: "Impact",
-        letterSpacing: 0.5,
+        fontFamily: "custom-font",
+        // fontWeight: 'bold',
+        letterSpacing: 1.5,
         backgroundColor: "white",
         width: "100%",
         justifyContent: "center",
