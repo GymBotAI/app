4234397bd5d2790c36fbc3e464c08cfb8b867f96
ShiGame45 <shiven.sheth@gmail.com>
2023-05-28 20:28:14 +0200

    Align semi works

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -9,11 +9,29 @@ import { bgPrimary } from "../styles";
 export default function AccountScreen({ navigation }) {
   return (
     <View
-      styles={{
-        backgroundColor: bgPrimary,
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: 'white',
       }}
     >
-      <StatusBar style="auto" />
+        <View
+        style={{
+            width: '50%',
+            height: '20%',
+            alignSelf: 'center',
+            marginTop: '75%',
+            backgroundColor: 'grey',
+        }}
+        >
+
+            <Text>
+
+            </Text>
+
+        </View>
+
+        <StatusBar style="auto" />
     </View>
   );
 }
