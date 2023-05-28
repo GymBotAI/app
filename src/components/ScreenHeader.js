7fd1f271537a62b0bb94b4c9129dfcea0839228f
ShiGame45 <shiven.sheth@gmail.com>
2023-05-28 20:10:37 +0200

    Added Navbar

--- src/components/ScreenHeader.js
+++ src/components/ScreenHeader.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, TouchableOpacity, Text, View } from "react-native";
 import * as Font from "expo-font";
 import React, { useState, useEffect } from "react";
 
@@ -28,20 +28,40 @@ export default function ScreenHeader({ text }) {
           paddingBottom: 5,
           backgroundColor: "white",
           width: "100%",
-          zIndex: 2,
+          display: 'flex',
+          flexDirection: 'row',
+          
         }}>
+      
+      <TouchableOpacity
+        style={{
+          padding: 10,
+          borderRadius: 5,
+          alignItems: "center",
+          paddingBottom: 12,
+        }}
+      >
+        <Image
+          source={require("../../assets/navbar.png")}
+          style={{
+            width: 30,
+            height: 30,
+          }}
+        />
+      </TouchableOpacity>
+
       <Text
         style={{
           fontSize: 56,
           color: "black",
           fontFamily: "custom-font",
           letterSpacing: 1.5,
-          textAlign: "center",
-          zIndex: 2,
+          flexBasis: 'flexEnd',
         }}
       >
         {text}
       </Text>
+
     </View>
   );
 }
