6594a3d6eb71b218bc8858ed9c972f184d7e747b
ShiGame45 <shiven.sheth@gmail.com>
2023-06-16 14:20:35 +0200

    tint

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,47 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import { ImageBackground, View, TextInput } from "react-native";
-import {useState} from "react";
+import { useState } from "react";
 
 import LoginBox from "../components/LoginBox";
-
-
-import { bgPrimary } from "../styles";
 
 export default function AccountScreen({ navigation }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-  
-    <ImageBackground
-      source={require("../../assets/accountbgdark.jpg")}
-      resizeMode="cover"
-      style={{
-        backgroundColor: 'white',
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-        <View
+    <View style={{ flex: 1, backgroundColor: 'blue'}}>
+      <ImageBackground
+        source={require("../../assets/accountbgdark.jpg")}
+        resizeMode="cover"
         style={{
-          width: '90%',
-          height: '32%',
-          alignSelf: 'center',
-          marginTop: 'auto',
-          marginBottom: 'auto',
-          backgroundColor: "rgba(245, 245, 245, 0.2)",
-          borderRadius: '15px',
+          width: "100%",
+          height: "100%",
+          opacity: .9, // Adjust the opacity as per your preference
         }}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
         >
-
-        <LoginBox></LoginBox>
-      
+          <View
+            style={{
+              width: "90%",
+              height: "32%",
+              backgroundColor: "rgba(245, 245, 245, 0.2)",
+              borderRadius: 15,
+            }}
+          >
+            <LoginBox />
+          </View>
         </View>
-
-        <StatusBar style="auto" />
-    </ImageBackground>
+      </ImageBackground>
+      <StatusBar style="auto" />
+    </View>
   );
 }
