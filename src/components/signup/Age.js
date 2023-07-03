6fb4105f9727142f3d9d5ca2d430482e7400eaea
ShiGame45 <shiven.sheth@gmail.com>
2023-07-03 21:35:22 +0200

    Age selection works!

--- src/components/signup/Age.js
+++ src/components/signup/Age.js
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
-import { TextInput } from "react-native";
+import { Text, View } from "react-native";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-// import DateTimePicker from "@react-native-community/datetimepicker";
+export default function Age() {
+  const [date, setDate] = useState(new Date()); // Set initial value to the current date
 
-export default function Age({ }) {
-    const [birthdate, setBirthdate] = useState(new Date()); // Set initial value to the current date
+  const onChange =(event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+    console.log(date)
+  }
 
   return (
-    <TextInput/>
-//     <DateTimePicker
-//     value={birthdate}
-//     mode="date"
-//     display="spinner"
-//     onChange={(event, selectedDate) => {
-//       if (selectedDate) {
-//         setBirthdate(selectedDate);
-//       }
-//     }}
-//   />
+    <View>
+
+        <DateTimePicker
+        testID="dateTimePicker"
+        value={date}
+        is24Hour={true}
+        display='default'
+        onChange={onChange}
+        />
+
+    </View>
   );
 }
