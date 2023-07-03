5d3d721cca6e2903e6ba40ccd8236be2e5b3a9e1
ShiGame45 <shiven.sheth@gmail.com>
2023-07-03 22:09:28 +0200

    DatePicker fully functions

--- src/components/signup/Age.js
+++ src/components/signup/Age.js
@@ -1,27 +1,73 @@
 import React, { useState } from "react";
-import { Text, View } from "react-native";
+import { TextInput, View, TouchableOpacity, StyleSheet, Pressable, Platform } from "react-native";
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function Age() {
-  const [date, setDate] = useState(new Date()); // Set initial value to the current date
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
 
-  const onChange =(event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-    console.log(date)
+  const toggleShow = () => {
+    setShowPicker(!showPicker)
+    console.log(showPicker)
+  }
+
+  const onChange = ({type}, selectedDate) => {
+    if (type == 'set') {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if (Platform.OS === 'android') {
+        toggleShow();
+      } else {
+        toggleShow();
+        setDateOfBirth(currentDate.toDateString())
+      }
+
+    } else {
+      toggleShow();
+    }
   }
 
   return (
     <View>
 
-        <DateTimePicker
-        testID="dateTimePicker"
+      {showPicker && 
+      (<DateTimePicker
+        mode='date'
+        display='spinner'
         value={date}
-        is24Hour={true}
-        display='default'
         onChange={onChange}
+      />)}
+
+      {!showPicker && (
+        <Pressable onPress={toggleShow}>
+
+        <TextInput 
+          style={styles.input}
+          placeholder="Sat Aug 21 2004"
+          value={dateOfBirth}
+          onChangeText={setDateOfBirth}
+          editable={false}
+          onPressIn={toggleShow}
         />
+
+      </Pressable>
+      )}
 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  dateText: {
+    fontSize: 24, // Adjust the font size as needed
+    textAlign: 'center',
+    marginTop: 20,
+  }, input: {
+    borderBottomWidth: 1,
+    paddingVertical: 10,
+    marginTop: 20,
+    fontSize: 20,
+  }
+});
