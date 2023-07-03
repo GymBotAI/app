712472e5ebf08a61d58044ad3fbc2ee54fc61b29
ShiGame45 <shiven.sheth@gmail.com>
2023-07-03 22:46:31 +0200

    Goals look more aesthetic

--- src/components/GoalSelect.js
+++ src/components/GoalSelect.js
@@ -1,43 +1,47 @@
 import React, { useState } from "react";
-import { Text, TouchableOpacity, StyleSheet, } from "react-native";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
 
+const GoalSelect = ({ text }) => {
+  const [selected, setSelected] = useState(false);
 
-export default function GoalSelect({ text, }) {
-    const [selected, setSelected] = useState(false)
-
-    const handlePress = () => {
-        setSelected(!selected);
-    };
+  const handlePress = () => {
+    setSelected(!selected);
+  };
 
   return (
-    <TouchableOpacity style={[styles.buttonBox, selected && styles.selectedButton]} onPress={handlePress}>
-    <Text style={styles.boxText}>{text}</Text>
-  </TouchableOpacity>
+    <TouchableOpacity
+      style={[styles.buttonBox, selected && styles.selectedButton]}
+      onPress={handlePress}
+    >
+      <Text style={[styles.boxText, selected && styles.selectedText]}>{text}</Text>
+    </TouchableOpacity>
   );
-}
+};
 
 const styles = StyleSheet.create({
   buttonBox: {
     margin: 10,
-    width: '45%',
+    width: "45%",
     borderWidth: 1,
-    borderColor: '#444',
+    borderColor: "#444",
     borderRadius: 10,
     padding: 10,
     paddingVertical: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
   selectedButton: {
     backgroundColor: "#ff9800", // Change to your desired background color when selected
-  },
-  boxImage: {
-    alignSelf: 'center',
-    width: 100,
-    height: 100,
-    marginBottom: 8,
+    borderColor: "#ff9800",
   },
   boxText: {
-    alignSelf: "center",
-    borderColor: "black",
     fontSize: 16,
+    color: "#444",
+    fontWeight: "bold",
+  },
+  selectedText: {
+    color: "#fff", // Change to your desired text color when selected
   },
 });
+
+export default GoalSelect;
--- src/components/signup/Goals.js
+++ src/components/signup/Goals.js
@@ -1,28 +1,43 @@
-import React, { useState } from "react";
-import { View } from "react-native";
+import React from "react";
+import { View, StyleSheet } from "react-native";
 
 import GoalSelect from "../GoalSelect";
 
-export default function Goals({ }) {
-
+const Goals = () => {
   return (
-    <>
-        <View style={{display: 'flex', flexDirection: 'row'}}>
-          <GoalSelect text="Build Muscle"/>
-          <GoalSelect text="Lose Weight"/>
-        </View>
-        <View style={{display: 'flex', flexDirection: 'row'}}>
-          <GoalSelect text="Run Faster"/>
-          <GoalSelect text="Better Abs"/>
-        </View>
-        <View style={{display: 'flex', flexDirection: 'row'}}>
-          <GoalSelect text="Jump Higher"/>
-          <GoalSelect text="Do a Pull Up"/>
-        </View>
-        <View style={{alignItems: 'center'}}>
-        <GoalSelect text="Other"/>
-        </View>
-
-    </>
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <GoalSelect text="Build Muscle" />
+        <GoalSelect text="Lose Weight" />
+      </View>
+      <View style={styles.row}>
+        <GoalSelect text="Run Faster" />
+        <GoalSelect text="Better Abs" />
+      </View>
+      <View style={styles.row}>
+        <GoalSelect text="Jump Higher" />
+        <GoalSelect text="Do a Pull Up" />
+      </View>
+      <View style={styles.centered}>
+        <GoalSelect text="Other" />
+      </View>
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
+  },
+  centered: {
+    alignItems: "center",
+  },
+});
+
+export default Goals;
