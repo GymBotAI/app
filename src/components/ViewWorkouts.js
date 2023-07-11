aa1bdbd0e50ffcd3d2406b9f3ac720a777fe72f3
Jigsaw <cuervoarangoguillermo@gmail.com>
2023-07-11 15:21:01 +0200

    Basic UI template (not yet finished)

--- src/components/ViewWorkouts.js
+++ src/components/ViewWorkouts.js
@@ -1,7 +1,28 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Image, Text, TouchableOpacity, StyleSheet, KeyboardAvoidingView, TextInput, } from "react-native";
+import Workouts from "./WorkoutTabs";
+import React, {useState} from "react"
 
 export default function ViewWorkouts({ navigation }) {
-  return <View style={styles.container}></View>;
+  const [task, setTask] = useState();
+
+  return( 
+   <View style={styles.container}>
+     <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Todays workouts:</Text>
+        <View style={styles.items}>
+          <Workouts text={"Leg Workout"}/>
+        </View>
+     </View>
+     <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding": "height"} style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={"Write a Workout"}/>
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+     </KeyboardAvoidingView>
+   </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -9,5 +30,47 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     overflow: "auto",
     padding: 10,
+    backgroundColor: "#F3F3F3"
+  },
+  tasksWrapper: {
+    paddingTop: 10,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width:60,
+    height:60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {
+
   },
 });
