6d4a2ccf8605aca10c3a98fa67841be2204feec6
ShiGame45 <shiven.sheth@gmail.com>
2023-07-11 19:02:33 +0200

    fixed prompt drop shadow

--- src/components/Prompts.js
+++ src/components/Prompts.js
@@ -11,6 +11,11 @@ const styles = {
     marginLeft: "auto",
     width: "100%",
     zIndex: 1,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
   },
   button: {
     marginLeft: "auto",
@@ -22,11 +27,6 @@ const styles = {
     borderWidth: 1.5,
     borderColor: "#5585f2",
     backgroundColor: "transparent",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 3,
   },
   text: {
     fontSize: 16,
