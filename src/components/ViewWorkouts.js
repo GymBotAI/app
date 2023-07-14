c11446efbabefc41a068320785199496fdb68684
Saw8888 <cuervoarangoguillermo@gmail.com>
2023-07-14 17:26:29 +0200

    Fixed some glichy button problems.

--- src/components/ViewWorkouts.js
+++ src/components/ViewWorkouts.js
@@ -29,73 +29,69 @@ export default function ViewWorkouts({ navigation }) {
   };
 
   return (
-    <ScrollView contentContainerStyle={styles.scrollContainer}>
-      <View style={styles.container}>
-        {!showAddWorkoutScreen && (
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "padding" : "height"}
-            style={styles.writeTaskWrapper}
-          >
-            <TouchableOpacity onPress={handleAddWorkout}>
-              <View style={styles.addWrapper}>
-                <Text style={styles.addText}>+</Text>
-              </View>
-            </TouchableOpacity>
-          </KeyboardAvoidingView>
-        )}
-        {!showAddWorkoutScreen && (
-          <View style={styles.tasksWrapper}>
-            <Text style={styles.sectionTitle}>Today's workouts:</Text>
-            {selectedWorkouts.map((workout, index) => (
-              <Workouts key={index} title={workout.title} text={workout.text} />
-            ))}
-          </View>
-        )}
-        {showAddWorkoutScreen && (
-          <AddWorkoutScreen
-            onClose={handleAddWorkoutScreenClose}
-            onWorkoutSelect={handleWorkoutSelect}
-          />
-        )}
-      </View>
-    </ScrollView>
-  );
+   <ScrollView contentContainerStyle={styles.scrollContainer}>
+     <View style={styles.container}>
+       {!showAddWorkoutScreen && (
+         <View style={styles.tasksWrapper}>
+           <Text style={styles.sectionTitle}>Today's workouts:</Text>
+           {selectedWorkouts.map((workout, index) => (
+             <Workouts key={index} title={workout.title} text={workout.text} />
+           ))}
+         </View>
+       )}
+       {!showAddWorkoutScreen && (
+         <View style={styles.addButtonContainer}>
+           <TouchableOpacity onPress={handleAddWorkout} style={styles.addButton}>
+             <Text style={styles.addText}>+</Text>
+           </TouchableOpacity>
+         </View>
+       )}
+       {showAddWorkoutScreen && (
+         <AddWorkoutScreen
+           onClose={handleAddWorkoutScreenClose}
+           onWorkoutSelect={handleWorkoutSelect}
+         />
+       )}
+     </View>
+   </ScrollView>
+ );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-  },
-  container: {
-    flex: 1,
-    padding: 10,
-    backgroundColor: "#F3F3F3",
-  },
-  tasksWrapper: {
-    paddingTop: 10,
-    paddingHorizontal: 20,
-    zIndex: 1,
-  },
-  sectionTitle: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  writeTaskWrapper: {
-    position: "absolute",
-    bottom: 60,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: "#FFF",
-    borderRadius: 60,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#C0C0C0",
-    borderWidth: 1,
-  },
+ scrollContainer: {
+   flexGrow: 1,
+ },
+ container: {
+   flex: 1,
+ },
+ tasksWrapper: {
+   paddingHorizontal: 20,
+   paddingBottom: 80,
+ },
+ sectionTitle: {
+   fontSize: 24,
+   fontWeight: "bold",
+   marginTop: 20,
+ },
+ addButtonContainer: {
+   position: "absolute",
+   bottom: 0,
+   left: 0,
+   right: 0,
+   alignItems: "center",
+   justifyContent: "center",
+   paddingBottom: 20,
+ },
+ addButton: {
+   width: 60,
+   height: 60,
+   borderRadius: 30,
+   backgroundColor: "blue",
+   alignItems: "center",
+   justifyContent: "center",
+ },
+ addText: {
+   color: "white",
+   fontSize: 30,
+ },
 });
