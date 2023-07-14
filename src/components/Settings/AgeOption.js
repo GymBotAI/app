a6c1bdd76ccf477e4af8a971a35d75545830b7f4
ShiGame45 <shiven.sheth@gmail.com>
2023-07-14 11:07:34 +0200

    changes to ageOption

--- src/components/Settings/AgeOption.js
+++ src/components/Settings/AgeOption.js
@@ -1,17 +1,6 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  StyleSheet,
-  TextInput,
-  Modal,
-} from "react-native";
-
+import { View, Text, Image, TouchableOpacity, StyleSheet, TextInput } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-
-
 import { circularColour } from "../../styles";
 
 export default function Option({ question, value, setValue }) {
@@ -19,33 +8,34 @@ export default function Option({ question, value, setValue }) {
   const [showPicker, setShowPicker] = useState(false);
 
   const show = () => {
-    console.log("This is happening")
+    console.log("This is happening");
     setShowPicker(true);
-  }
+  };
 
-  const onChange = ({ type }, selectedDate) => {
-    if (type === "set") {
-      setValue(selectedDate)
+  const onChange = (_, selectedDate) => {
+    setShowPicker(false);
+    if (selectedDate) {
+      setValue(selectedDate);
     }
   };
 
-
-  console.log(showPicker)
+  console.log(showPicker);
   return (
-
-    
-    <TouchableOpacity style={styles.item} onPress={show}>
-      <Text style={styles.itemText}>{question}</Text>
-      <View style={styles.itemLeft}>
-        <TextInput style={styles.current} value={value.toLocaleDateString()} onChange={setValue}/>
-        <Image
-              source={require("../../../assets/edit.png")}
-              style={{width: 22, height: 22, marginLeft: 15,}}
-              resizeMode="contain"
-            />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.item} onPress={show}>
+        <Text style={styles.itemText}>{question}</Text>
+        <View style={styles.itemLeft}>
+          <TextInput style={styles.current} value={value.toLocaleDateString()} />
+          <Image
+            source={require("../../../assets/edit.png")}
+            style={{ width: 22, height: 22, marginLeft: 15 }}
+            resizeMode="contain"
+          />
         </View>
-        {showPicker && (
+      </TouchableOpacity>
+      {showPicker && (
         <DateTimePicker
+          testID="dateTimePicker"
           mode="date"
           display="spinner"
           value={value}
@@ -54,39 +44,41 @@ export default function Option({ question, value, setValue }) {
           maximumDate={new Date(2030, 0, 1)} // Replace with your maximum date (e.g., new Date() for today)
         />
       )}
-      
-      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-    item: {
-      backgroundColor: "#FFF",
-      padding: 15,
-      borderRadius: 10,
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: 'space-between',
-      marginBottom: 15,
-    },
-    itemText: {
-      maxWidth: "100%",
-      fontSize: 16,
-    },
-    itemLeft: {
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: "wrap",
-    },
-    current: {
-      color: '#1260de',
-      fontSize: 16,
-    },
-    circular: {
-      width: 12,
-      height: 12,
-      borderColor: circularColour,
-      borderWidth: 2,
-      borderRadius: 5,
-    },
-  });  
+  container: {
+    flex: 1,
+  },
+  item: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 15,
+  },
+  itemText: {
+    maxWidth: "100%",
+    fontSize: 16,
+  },
+  itemLeft: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  current: {
+    color: "#1260de",
+    fontSize: 16,
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: circularColour,
+    borderWidth: 2,
+    borderRadius: 5,
+  },
+});
