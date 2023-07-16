45d7345e310e226f3d9e6241c2d42da047848932
ShiGame45 <shiven.sheth@gmail.com>
2023-07-16 11:32:39 +0200

    Set max character length to Name

--- src/components/SignUp/Name.js
+++ src/components/SignUp/Name.js
@@ -1,18 +1,21 @@
 import React, { useState, useRef, useEffect } from "react";
-import { TextInput, View, Keyboard } from "react-native";
-
-import * as Device from "expo-device";
+import { TextInput, View, Keyboard, Text } from "react-native";
 
 export let nameValue = ""; // Export the nameValue variable
 
 export default function Name({ onNameChange }) {
   const [name, setName] = useState("");
+  const [charactersLeft, setCharactersLeft] = useState(15);
 
   const handleNameChange = (text) => {
     setName(text);
     onNameChange(text !== ""); // Update the parent component with input status
     nameValue = text;
+
+    const remainingCharacters = 15 - text.length; // Calculate the number of characters left
+    setCharactersLeft(remainingCharacters);
   };
+
   return (
     <View style={{ flexGrow: 1, overflow: "auto" }}>
       <TextInput
@@ -28,8 +31,12 @@ export default function Name({ onNameChange }) {
         }}
         placeholder="Enter your name"
         value={name}
+        maxLength={15}
         onChangeText={handleNameChange}
       />
+      <View style={{ alignItems: "flex-end", marginRight: 20 }}>
+        <Text style={{ color: "#888" }}>{charactersLeft}</Text>
+      </View>
     </View>
   );
 }
