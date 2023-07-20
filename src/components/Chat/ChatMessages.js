30ed4778704f12927e2554e268c55ec3de1e288d
ShiGame45 <shiven.sheth@gmail.com>
2023-07-20 22:33:22 +0200

    Prompts work :pray:

--- src/components/Chat/ChatMessages.js
+++ src/components/Chat/ChatMessages.js
@@ -37,6 +37,21 @@ export default function ChatMessages({
   };
 
   useEffect(() => {
+
+    for (let i=0; i < Goals.length; i++) {
+      Goals[i] = Goals[i].toLowerCase()
+      if (Goals[i] === "better abs") {
+        Goals[i] = ("How do I get "+Goals[i]+"?")
+      } else if (Goals[i] === "do a pull up") {
+        Goals[i] = ("How can I "+Goals[i]+"?")
+      } else if (Goals[i] === "other") {
+        Goals[i] = ("")
+      } else {
+        Goals[i] = ("How do I "+Goals[i]+"?")
+      }
+      console.log(Goals[i])
+    }
+
     const showListener = Keyboard.addListener(
       Device.osName == "Android" ? "keyboardDidShow" : "keyboardWillShow",
       () => {
