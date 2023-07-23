48a95e6d758b8220acf263d229051852313ed772
ShiGame45 <shiven.sheth@gmail.com>
2023-07-23 20:16:11 +0200

    Background on design workout button

--- src/components/Workout/CreateWorkoutButton.js
+++ src/components/Workout/CreateWorkoutButton.js
@@ -1,62 +1,56 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-} from "react-native";
+import React from "react";
+import { View, TouchableOpacity, Text, ImageBackground, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function CreateWorkoutButton({ handleAddWorkout }) {
-
   return (
-        <View style={styles.addButtonContainer}>
-            <TouchableOpacity
-              onPress={() => {
-                handleAddWorkout();
-              }}
-              style={styles.addButton}
-            >
-              <Text style={styles.addText}>+</Text>
-            </TouchableOpacity>
-          </View>
+    <TouchableOpacity
+      style={styles.addButtonContainer}
+      onPress={handleAddWorkout}
+    >
+
+    
+        {/* <LinearGradient colors={["blue", "black"]} style={{ flex: 1 }}>
+                <View style={{ flex: 1 }} />
+                </ImageBackground> */}
+
+    <LinearGradient colors={["#99a8ff", "#193bff"]} style={styles.blueRectangle}>
+
+    <ImageBackground
+                source={require("../../../assets/dumbellbg.webp")}
+                resizeMode="cover"
+                style={{
+                    width: "120%",
+                    height: "123%",
+                    marginLeft: -15,
+                    opacity: 0.3, // Adjust the opacity as per your preference
+                }}
+                />
+        <Text style={styles.addText}>Design your workout</Text>
+      </LinearGradient>
+
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    flexGrow: 1,
-  },
-  container: {
-    flex: 1,
-  },
-  tasksWrapper: {
-    paddingHorizontal: 20,
-    paddingBottom: 80,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
   addButtonContainer: {
-    // position: "absolute",
-    // bottom: 0,
-    // left: 0,
-    // right: 0,
     alignItems: "center",
     justifyContent: "center",
     paddingBottom: 20,
   },
-  addButton: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center",
+  blueRectangle: {
+    borderRadius: 15,
+    padding: 15,
+    paddingBottom: 13,
+    width: '92%',
+    height: 160,
+    alignItems: "left",
+    justifyContent: "flex-end",
   },
   addText: {
     color: "white",
-    fontSize: 30,
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
