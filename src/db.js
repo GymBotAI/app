478b36eaeccaef73c091aea5bcfe6100dc6f54a9
ShiGame45 <shiven.sheth@gmail.com>
2023-08-07 14:41:07 -0400

    db in progress

---
+++ src/db.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+
+async function handleInsert (name) {
+    console.log("Name is",name)
+    try {
+        const response = await axios.post('http://openhost.ddns.net', {
+            username: name,
+            password: '12345678',
+            email: 'bob@gmail.com',
+        });
+    
+        if (response.data.success) {
+            Alert.alert('Record inserted successfully');
+        } else {
+            Alert.alert('Failed to insert record');
+        }
+        } catch (error) {
+        console.error('Error inserting record:', error);
+        }
+    };
+
+export default handleInsert;
