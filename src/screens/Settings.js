35e703670345771b96ce86299723abd895637d28
Saw8888 <cuervoarangoguillermo@gmail.com>
2023-08-19 17:06:24 +0200

    Added Notifications bell in settings

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,7 +1,7 @@
-import { View, StatusBar } from "react-native";
+import { View, StatusBar, StyleSheet, Text} from "react-native";
 
 import EditSettings from "../components/Settings/EditSettings";
-import ScreenHeader from "../components/ScreenHeader";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function Settings({ navigation }) {
   return (
@@ -11,10 +11,43 @@ export default function Settings({ navigation }) {
         width: "100%",
       }}
     >
-      <ScreenHeader title="Settings" />
+      <View style={styles.container}>
+        <Text style={styles.title}>{"Settings"}</Text>
+        <View style={styles.bellContainer}>
+          <FontAwesome5 name = "bell" size = {30} colour = "#8ed9de" />
+        </View>
+      </View>
       <EditSettings navigation={navigation}/>
       <StatusBar barStyle="dark" />
 
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingTop: 40,
+    height: 90,
+    borderWidth: 0.5,
+    borderColor: "#c9c9c9",
+  },
+ icon: {
+  name: "bell",
+  size: 30,
+  colour: "#8ed9de",
+ },
+  title: {
+    flex: 1,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+    paddingLeft: 30,
+  },
+  bellContainer: {
+    paddingRight: 15,
+  },
+});
