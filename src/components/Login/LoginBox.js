9a9f964c108b78af8d45f948d4235a5fa860d886
die-lolol <die.lolol.1.0@gmail.com>
2023-08-22 12:04:07 -0400

    Added temporary login functionality (Email: One, Password: Two)

--- src/components/Login/LoginBox.js
+++ src/components/Login/LoginBox.js
@@ -10,6 +10,7 @@ import { Image } from "expo-image";
 
 
 import handleInsert from "../../db";
+import { username } from "../SignUp/Credentials";
 
 var pass = "";
 
@@ -26,34 +27,43 @@ export default function LoginBox({ navigation }) {
     }
   };
 
-  const checkCredentials = () => {
-    fetch("http://openhost.ddns.net:3000/select",{
-      method:"POST",
-      headers:{
-        "Content-Type": "application/json",
-      },
-      body:JSON.stringify({
-        "USERNAME": {username},
-        "PASSWORD": {password}
-      }) 
-    })
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data.USERNAME)
-      pass = data.PASSWORD
-    })
-    if(pass === password) {
-      navigation.navigate("Home");
-    } else {
-      //say wrong password
-      console.log(pass)
-    } //it doesnt work yet
-  }
+  const checkCredentials = async (email, password) => {
+    try {
+      const response = await fetch("http://openhost.ddns.net:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          "username": String(email),
+          "password": String(password)
+        }),
+      });
+  
+      const data = await response.json();
+      return data; // Return the response data
+    } catch (error) {
+      console.error("Error checking credentials:", error);
+      return null; // Return null in case of an error
+    }
+  };
+  
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     // onPressInsert()
-    // checkCredentials()
-    navigation.navigate("Home");
+    console.log(email)
+    console.log(password)
+    console.log("Login result:")
+    const result = await checkCredentials(email, password);
+    console.log(result)
+    if (result && result.message === "Login successful") { //for testing use One for email and Two for password
+      navigation.navigate("Home");
+    } else if (result && result.message === "Invalid credentials") {
+      console.log("Invalid login attempt")
+    } else {
+      console.log("Error")
+    }
+    //navigation.navigate("Home");
   };
 
   const handleCreateAccount = () => {
@@ -73,6 +83,7 @@ export default function LoginBox({ navigation }) {
         value={email}
         onChangeText={setEmail}
         autoCapitalize = 'none'
+        secureTextEntry
       />
       <TextInput
         style={styles.input}
