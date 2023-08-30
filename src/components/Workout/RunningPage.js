b9e18d72f302632ee03580e3679cd15f30dfe508
Saw8888 <cuervoarangoguillermo@gmail.com>
2023-08-30 21:17:48 +0200

    Added THE WHOLE FUCKING GEOLOCATION PAGE LEZ GOOOOOO :)

--- src/components/Workout/RunningPage.js
+++ src/components/Workout/RunningPage.js
@@ -1,31 +1,78 @@
-import React from 'react';
-import { View, StyleSheet, Modal, Dimensions, TouchableOpacity, Text, Time } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Modal, Dimensions, TouchableOpacity, Text, Alert } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
+import MapAndPos from './runningMap&Pos';
 
 export default function CustomPage({ isVisible, onClose }) {
+  const [kilometers, setKilometers] = useState(0);
+  const [StepsTaken, setStepsTaken] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [calibratedBool, setcalibratedBool] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const updateTotalDistance = (distance) => {
+    setKilometers(distance);
+  };
+
+  const updateCalibratedBool = (calibrated) => {
+    setcalibratedBool(true);
+  }
+
   const windowWidth = Dimensions.get('window').width;
 
-  let StepsTaken = 500;
-  let Kilometers = 1;
-  let time = new Date().getHours() + ":" + new Date().getMinutes()+ ":"+new Date().getSeconds();
+  let hours, mins, secs,nhours,nmins,nsecs;
+  let finalH, finalM, finalS;
+
+  const StartAndEndTime = () => {
+    if(timerStarted === false){
+      console.log("Timer has started");
+      hours = new Date().getHours();
+      mins = new Date().getMinutes();
+      secs = new Date().getSeconds();
+      setTimerStarted(true);
+    } else {
+      nhours = new Date().getHours();
+      nmins = new Date().getMinutes();
+      nsecs = new Date().getSeconds();
+      finalH = nhours - hours;
+      finalM = nmins - mins;
+      finalS = nsecs - secs;
+      console.log("Timer has ended. Time:");
+      console.log(finalH, ":", finalM, ":", finalS);
+    }
+  };
 
   return (
-    <Modal
-      animationType="slide"
-      transparent={true}
-      visible={isVisible}
-    >
+    <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View style={[styles.centeredView, { width: windowWidth }]}>
         <TouchableOpacity style={styles.closeButton} onPress={onClose}>
           <FontAwesome5 name="long-arrow-alt-left" size={24} color="#adadad" />
         </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.toggleButton}
+          onPress={() => {
+            if (calibratedBool) {
+              setIsActive(!isActive);
+              StartAndEndTime();
+            } else {
+              Alert.alert(
+                'Error',
+                'You cannot start a run while calibration is occurring, this will at most take 1 min',
+                { text: 'OK', onPress: () => console.log('OK Pressed') }
+              );
+            }
+          }}
+          >
+          <FontAwesome5 name="arrow-right" size={24} color={calibratedBool && isActive ? "green" : "black"} />
+        </TouchableOpacity>
+
         <View style={styles.modalView}>
           <View style={styles.container}>
-           <Text style={styles.mapNotThereText}>Insert Map Here</Text>
+            <MapAndPos updateTotalDistance={updateTotalDistance} updateCalibrated={updateCalibratedBool} isActive={isActive} />
           </View>
           <Text style={styles.redText}>Steps: {StepsTaken}</Text>
-          <Text style={styles.greenText}>Km's: {Kilometers}</Text>
-          <Text style={styles.blueText}>Time: {time}</Text>
+          <Text style={styles.greenText}>Km's: {kilometers}</Text>
+          <Text style={styles.blueText}>Time {finalH ? finalH : 0} : {finalM ? finalM : 0} : {finalS ? finalS : 0}</Text>
         </View>
       </View>
     </Modal>
@@ -84,9 +131,15 @@ const styles = StyleSheet.create({
     zIndex: 1,
   },
   mapNotThereText: {
-   textAlign: "center",
-   paddingTop: 150,
-   fontSize: 40,
-   color: '#adadad'
+    textAlign: "center",
+    paddingTop: 150,
+    fontSize: 40,
+    color: '#adadad'
+  },
+  toggleButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    zIndex: 1,
   },
 });
