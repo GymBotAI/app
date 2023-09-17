a71a768e90798ea73a9e801484bf0b88b88a7f89
lafkpages <soy.lafk+gh@gmail.com>
2023-09-17 16:36:21 +0200

    Remove unnecessary fragment in ChatInput

--- src/components/Chat/ChatInput.js
+++ src/components/Chat/ChatInput.js
@@ -56,64 +56,62 @@ export default function ChatInput({
   };
 
   return (
-    <>
-      <View
+    <View
+      style={{
+        borderTopWidth: 1,
+        borderColor: "#E2E2E2",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 8,
+        paddingHorizontal: 15,
+        backgroundColor: "#F5F5F5",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: -2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 1,
+        elevation: 2,
+      }}
+    >
+      <TextInput
+        blurOnSubmit={false}
+        multiline={typeof multiline === "boolean" ? multiline : false}
+        inputMode="text"
+        returnKeyType={returnKeyType || "send"}
+        numberOfLines={4}
         style={{
-          borderTopWidth: 1,
-          borderColor: "#E2E2E2",
-          flexDirection: "row",
-          alignItems: "center",
+          flex: 1,
+          borderWidth: 1,
+          borderColor: "#D8D8D8",
+          borderRadius: 20,
+          backgroundColor: "white",
+          paddingHorizontal: 12,
           paddingVertical: 8,
-          paddingHorizontal: 15,
-          backgroundColor: "#F5F5F5",
-          shadowColor: "#000",
-          shadowOffset: { width: 0, height: -2 },
-          shadowOpacity: 0.1,
-          shadowRadius: 1,
-          elevation: 2,
+          fontFamily: fontLoaded ? "roboto-regular" : null,
+          fontSize: 16,
+          maxHeight: 100,
         }}
-      >
-        <TextInput
-          blurOnSubmit={false}
-          multiline={typeof multiline === "boolean" ? multiline : false}
-          inputMode="text"
-          returnKeyType={returnKeyType || "send"}
-          numberOfLines={4}
-          style={{
-            flex: 1,
-            borderWidth: 1,
-            borderColor: "#D8D8D8",
-            borderRadius: 20,
-            backgroundColor: "white",
-            paddingHorizontal: 12,
-            paddingVertical: 8,
-            fontFamily: fontLoaded ? "roboto-regular" : null,
-            fontSize: 16,
-            maxHeight: 100,
-          }}
-          value={text}
-          onChange={(e) => setText(e.nativeEvent.text)}
-          onSubmitEditing={handleOnSubmit}
-        />
+        value={text}
+        onChange={(e) => setText(e.nativeEvent.text)}
+        onSubmitEditing={handleOnSubmit}
+      />
 
-        <TouchableOpacity
-          style={{
-            marginLeft: 16,
-            borderRadius: 12,
-            backgroundColor: "#1877F2",
-            padding: 6.2,
-            paddingHorizontal: 8,
-            paddingRight: 6,
-          }}
-          onPress={handleOnSubmit}
-        >
-          <Image
-            source={require("../../../assets/send.png")}
-            style={{ height: 22, width: 22 }}
-          />
-          {/* <AntDesign name="arrowright" size={24} color="white" /> */}
-        </TouchableOpacity>
-      </View>
-    </>
+      <TouchableOpacity
+        style={{
+          marginLeft: 16,
+          borderRadius: 12,
+          backgroundColor: "#1877F2",
+          padding: 6.2,
+          paddingHorizontal: 8,
+          paddingRight: 6,
+        }}
+        onPress={handleOnSubmit}
+      >
+        <Image
+          source={require("../../../assets/send.png")}
+          style={{ height: 22, width: 22 }}
+        />
+        {/* <AntDesign name="arrowright" size={24} color="white" /> */}
+      </TouchableOpacity>
+    </View>
   );
 }
