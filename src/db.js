6acecb11e92c14371701f8aa68ef46f3e2c57dc6
lafkpages <soy.lafk+gh@gmail.com>
2023-09-17 16:51:50 +0200

    Don't use Axios in DB

--- src/db.js
+++ src/db.js
@@ -1,13 +1,17 @@
-import axios from "axios";
-
 async function handleInsert(name) {
   console.log("Name is", name);
   try {
-    const response = await axios.post("http://openhost.ddns.net", {
-      username: name,
-      password: "12345678",
-      email: "bob@gmail.com",
-    });
+    const response = await fetch("http://openhost.ddns.net", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: name,
+        password: "12345678",
+        email: "bob@gmail.com",
+      }),
+    }).then((response) => response.json());
 
     if (response.data.success) {
       Alert.alert("Record inserted successfully");
