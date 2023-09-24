b18a6ef184585842627d5676fa02d3db5423d4a3
Gui <cuervoarangoguillermo@gmail.com>
2023-09-24 18:30:26 +0200

    Added some functionality

--- src/components/Plans/PlansContainer.js
+++ src/components/Plans/PlansContainer.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Settings({ navigation }) {
+ const [planFinderVisibility, setPlanFinderVisibility] = useState(false)
+
+ const openPlanFinder = () => {
+  setPlanFinderVisibility(!planFinderVisibility)
+ }
+
+
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
+     <TouchableOpacity style={styles.addButtonContainer} onPress={openPlanFinder}>
+        <Text style={styles.addText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,4 +24,25 @@ const styles = StyleSheet.create({
     overflow: "auto",
     padding: 10,
   },
+  addText: {
+   alignSelf: "center",
+   fontSize: 23,
+   fontWeight: "bold",
+   paddingVertical: 10,
+   color: "#dbdbdb",
+ },
+ addButtonContainer: {
+  position: "absolute",
+  bottom: 40,
+  left: "50%",
+  marginLeft: -22,
+  width: 50,
+  backgroundColor: "#1260de",
+  borderRadius: 40,
+  shadowColor: "black",
+  shadowOffset: { width: 0, height: 10 },
+  shadowOpacity: 0.6,
+  shadowRadius: 10,
+  elevation: 5,
+},
 });
--- src/components/Workout/WorkoutContainer.js
+++ src/components/Workout/WorkoutContainer.js
@@ -14,10 +14,12 @@ import CustomPage from "./RunningPage";
 export default function ViewWorkouts({ navigation }) {
   const [selectedWorkouts, setSelectedWorkouts] = useState([]);
   const [RunningPageVisibility, changeRunningPageVisibility] = useState(false);
+  const [workoutButtonsVisible, setWorkoutButtonsVisible] = useState(false);
 
   const openRunningPage = () => {
     changeRunningPageVisibility(true);
   };
+
   const closeRunningPage = () => {
     changeRunningPageVisibility(false);
   };
@@ -26,20 +28,34 @@ export default function ViewWorkouts({ navigation }) {
     navigation.navigate("DesignWorkout");
   };
 
+  const toggleWorkoutButtons = () => {
+    setWorkoutButtonsVisible(!workoutButtonsVisible);
+  };
+
   return (
     <>
-    <ScrollView style={styles.container}>
-      <CreateWorkoutButton handleAddWorkout={handleAddWorkout} />
+      <ScrollView style={styles.container}>
+        <CreateWorkoutButton handleAddWorkout={handleAddWorkout} />
 
-      <TalkToGymBotSection navigation={navigation} />
-      <WorkoutList navigation={navigation}/>
-      <CustomPage isVisible={RunningPageVisibility} onClose={closeRunningPage}/>
-    </ScrollView>
-    
-      {/* Log Workout button */}
-      <TouchableOpacity style={styles.addButtonContainer} onPress={openRunningPage}>
-          <Text style={styles.addText}>Log Workout</Text>
+        <TalkToGymBotSection navigation={navigation} />
+        <WorkoutList navigation={navigation} />
+        <CustomPage isVisible={RunningPageVisibility} onClose={closeRunningPage} />
+      </ScrollView>
+
+      <TouchableOpacity style={styles.addButtonContainer} onPress={toggleWorkoutButtons}>
+        <Text style={styles.addText}>+</Text>
       </TouchableOpacity>
+
+      {workoutButtonsVisible && (
+        <View style={styles.workoutButtonsContainer}>
+          <TouchableOpacity onPress={openRunningPage}>
+            <Text style={styles.workoutOptionsText}>Start a Run</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.workoutOptionsText}>Log Workout</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </>
   );
 }
@@ -49,18 +65,11 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 20,
   },
-  tasksWrapper: {
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
   addButtonContainer: {
     position: "absolute",
     bottom: 80,
-    width: "86%",
-    marginLeft: '7%',
+    width: "1%",
+    marginLeft: "7%",
     backgroundColor: "#1260de",
     borderRadius: 8,
     shadowColor: "black",
@@ -69,11 +78,31 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 5,
   },
+  workoutButtonsContainer: {
+    position: "absolute",
+    bottom: 130,
+    marginLeft: "7%",
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.6,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  workoutOptionsText: {
+   alignSelf: "center",
+   fontSize: 13,
+   fontWeight: "bold",
+   paddingVertical: 10,
+   color: "#000",
+ },
   addText: {
     alignSelf: "center",
     fontSize: 23,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     paddingVertical: 10,
     color: "#dbdbdb",
   },
 });
+
