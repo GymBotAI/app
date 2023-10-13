fc7216c23a1b0f9251ab41f3092679fb3338a2b9
lafkpages <soy.lafk+gh@gmail.com>
2023-10-13 13:39:53 +0200

    Styles better export

--- src/styles.ts
+++ src/styles.ts
@@ -1,32 +1,16 @@
-const bgPrimary = "#4DBCC1";
-const bgMenu = "#408589";
-const borderDefault = "#FFFFFF";
-const white = "#FFFFFF";
-const fontSize = 16;
-const font = "Georgia";
-const circularColour = "#55BCF6";
-const minHeight = 40;
-const maxHeight = 250;
-const minWeight = 30;
-const maxWeight = 500;
+export const bgPrimary = "#4DBCC1";
+export const bgMenu = "#408589";
+export const borderDefault = "#FFFFFF";
+export const white = "#FFFFFF";
+export const fontSize = 16;
+export const font = "Georgia";
+export const circularColour = "#55BCF6";
+export const minHeight = 40;
+export const maxHeight = 250;
+export const minWeight = 30;
+export const maxWeight = 500;
 
-const maxDate = new Date();
+export const maxDate = new Date();
 maxDate.setFullYear(maxDate.getFullYear() - 12);
 
-const minDate = new Date(1900, 0, 1);
-
-export {
-  bgPrimary,
-  bgMenu,
-  borderDefault,
-  white,
-  fontSize,
-  font,
-  circularColour,
-  minHeight,
-  maxHeight,
-  minWeight,
-  maxWeight,
-  maxDate,
-  minDate,
-};
+export const minDate = new Date(1900, 0, 1);
