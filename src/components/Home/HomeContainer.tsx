e3b960978fa7b0b173fd13810928935c260e2b44
lafkpages <soy.lafk+gh@gmail.com>
2023-10-19 20:18:44 +0200

    HomeContainer settings btn

--- src/components/Home/HomeContainer.tsx
+++ src/components/Home/HomeContainer.tsx
@@ -15,48 +15,19 @@ import WorkoutStats from "./WorkoutStats";
 import WorkoutList from "./WorkoutList";
 import WorkoutPreview from "./WorkoutPreview";
 
-export let nameSetting = "";
-export let ageSetting = "";
-export let weightSetting = "";
-export let heightSetting = "";
-export let genderSetting = "";
-
-export default function HomeScreen({ navigation }) {
-  const appContext = useContext(AppContext);
-
-  const getDetails = async (emailVal: string) => {
-    // try {
-    //   navigation.navigate("Settings");
-    //   const response = await fetch("http://openhost.ddns.net:3000/search", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       USERNAME: String(emailVal),
-    //     }),
-    //   });
-    //   const data = await response.json();
-    //   nameSetting = data[0].NAME;
-    //   ageSetting = data[0].AGE;
-    //   genderSetting = data[0].GENDER;
-    //   weightSetting = data[0].WEIGHT;
-    //   heightSetting = data[0].HEIGHT;
-    //   navigation.navigate("Settings");
-    //   //return data;
-    // } catch (error) {
-    //   console.error("An error occurred while fetching data:", error);
-    // }
-  };
+export default function HomeContainer({ navigation }) {
+  const { session } = useContext(AppContext);
 
   return (
     <ScrollView style={styles.container}>
       {/* Top Section */}
       <View style={styles.topSection}>
         <Text style={styles.greetingText}>
-          Hello, {appContext.session?.user.email}
+          Hello, {session?.user.email}
         </Text>
-        <TouchableOpacity onPress={() => getDetails(/*emailValue*/ "")}>
+        <TouchableOpacity onPress={() => {
+          navigation.navigate("Settings");
+        }}>
           <FontAwesome5 name="cog" size={28} color="#333" />
           {/* <Image source={require("../../../assets/GymBotLogo.jpg")} style={{
     width: 50, height: 50, borderRadius: 22}}/> */}
