f79ba3210582aaa891465d508f5c50394a5e2ce6
lafkpages <soy.lafk+gh@gmail.com>
2023-10-20 19:02:56 +0200

    Remove old stuff from EditSettings

--- src/components/Settings/EditSettings.tsx
+++ src/components/Settings/EditSettings.tsx
@@ -12,53 +12,46 @@ import AgeOption from "./AgeOption";
 import GenderOption from "./GenderOption";
 import WeightOption from "./HeightWeightOption";
 
-import { nameSetting } from "../Home/HomeContainer";
-import { ageSetting } from "../Home/HomeContainer";
-import { genderSetting } from "../Home/HomeContainer";
-import { weightSetting } from "../Home/HomeContainer";
-import { heightSetting } from "../Home/HomeContainer";
-import { emailValue } from "../Login/LoginBox";
-
 import { minHeight } from "../../styles";
 import { maxHeight } from "../../styles";
 import { minWeight } from "../../styles";
 import { maxWeight } from "../../styles";
 
 export default function Settings({ navigation }) {
-  const [name, setName] = useState(nameSetting);
+  const [name, setName] = useState('your name (TODO)');
   const [newName, setNewName] = useState("");
-  const [bday, setBday] = useState(ageSetting);
-  const [gender, setGender] = useState(genderSetting);
-  const [weight, setWeight] = useState(weightSetting);
-  const [height, setHeight] = useState(heightSetting);
+  const [bday, setBday] = useState('your birthday (TODO)');
+  const [gender, setGender] = useState('your gender (TODO)');
+  const [weight, setWeight] = useState('your weight (TODO)');
+  const [height, setHeight] = useState('your height (TODO)');
 
   const handleSaveChanges = () => {
     // Perform saving changes logic here
   };
 
   const updateCredentials = async () => {
-    try {
-      const response = await fetch("http://openhost.ddns.net:3000/edit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          NAME: String(name),
-          USERNAME: emailValue,
-          GENDER: gender,
-          DOB: bday,
-          WEIGHT: weight,
-          HEIGHT: height,
-        }),
-      });
+    // try {
+    //   const response = await fetch("http://openhost.ddns.net:3000/edit", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       NAME: String(name),
+    //       USERNAME: emailValue,
+    //       GENDER: gender,
+    //       DOB: bday,
+    //       WEIGHT: weight,
+    //       HEIGHT: height,
+    //     }),
+    //   });
 
-      const data = await response.json();
-      return data; // Return the response data
-    } catch (error) {
-      console.error("Error checking credentials:", error);
-      return null; // Return null in case of an error
-    }
+    //   const data = await response.json();
+    //   return data; // Return the response data
+    // } catch (error) {
+    //   console.error("Error checking credentials:", error);
+    //   return null; // Return null in case of an error
+    // }
   };
 
   const handleGoHome = async () => {
@@ -67,14 +60,14 @@ export default function Settings({ navigation }) {
     setName(name);
     console.log(newName);
     updateCredentials();
-    console.log(ageSetting);
+    // console.log(ageSetting);
   };
 
   return (
     <Pressable style={styles.container} onPress={Keyboard.dismiss}>
       {/* <View style={styles.container}> */}
 
-      <WeightOption question="Name" value={name} setValue={setName} />
+      {/* <WeightOption question="Name" value={name} setValue={setName} /> */}
       <AgeOption question="Age" value={bday} setValue={setBday} />
       <GenderOption question="Gender" value={gender} setValue={setGender} />
       <WeightOption
