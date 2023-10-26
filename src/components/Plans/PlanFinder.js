584ab3385d82837a8ba9129c00c7c7dcf4732287
Gui <cuervoarangoguillermo@gmail.com>
2023-10-26 21:30:56 +0200

    This Iis the most fucking painfull commit. I spent 1h only to find 3 lines that fucked everything up.

--- src/components/Plans/PlanFinder.js
+++ src/components/Plans/PlanFinder.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   StyleSheet,
@@ -7,122 +7,123 @@ import {
   Text,
   TextInput,
   ScrollView,
+  Animated,
 } from "react-native";
 import PlanTabs from "./PlanTabs";
-import PlanInfoPage from "./PlanInfoPage";
 import plans from "./plans";
+import PlanInfoPage from "./PlanInfoPage"
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function PlanFinder() {
   const [searchQuery, setSearchQuery] = useState("");
   const [PlanInfoPageVisibility, setPlanInfoPageVisibility] = useState(false);
-  const [selectedPlans, setSelectedPlans] = useState([]);
+  const [isScrolling, setIsScrolling] = useState(false);
   const [planInfo, setPlanInfo] = useState({});
 
-  const onAddPlan = (plan) => {
-    setSelectedPlans([...selectedPlans, plan]);
+  const onScroll = (event) => {
+    setIsScrolling(event.nativeEvent.contentOffset.y > 0);
   };
 
   const filteredPlans = plans.filter((plan) =>
     plan.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const togglePlanInfoPageVisibility = (circleCount, plan) => {
+  const togglePlanInfoPageVisibility = (plan) => {
     setPlanInfoPageVisibility(!PlanInfoPageVisibility);
     setPlanInfo(plan);
   };
 
   return (
-   <View style={styles.container}>
-     <View style={styles.overlay} />
-     <View style={styles.searchBarContainer}>
-       <TextInput
-         style={styles.searchBar}
-         placeholder="Search plans"
-         placeholderTextColor="#888"
-         onChangeText={(text) => setSearchQuery(text)}
-       />
-     </View>
-     <ScrollView style={[styles.tabsContainer, PlanInfoPageVisibility ? { position: "absolute" } : {}]}>
-       {filteredPlans.map((plan, index) => (
-         <TouchableOpacity
-           onPress={() => togglePlanInfoPageVisibility(plan.length, plan)}
-           key={index}
-         >
-           <PlanTabs
-             text={plan.name}
-             length={plan.length}
-             data={plan.data}
-             imageSource={require("../../../assets/accountbgdark.jpg")}
-           />
-         </TouchableOpacity>
-       ))}
-     </ScrollView>
-     {PlanInfoPageVisibility && (
-       <PlanInfoPage
-         onClose={togglePlanInfoPageVisibility}
-         onAddPlan={onAddPlan}
-         windowWidth={windowWidth}
-         windowHeight={windowHeight}
-         fillPercentage={50}
-         plan={planInfo}
-       />
-     )}
-   </View>
- );
+    <View style={styles.container}>
+      <View style={styles.overlay} />
+      <View
+        style={[
+          styles.searchBarContainer,
+          isScrolling ? styles.searchBarHidden : null,
+        ]}
+      >
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search plans"
+          placeholderTextColor="#888"
+          onChangeText={(text) => setSearchQuery(text)}
+        />
+      </View>
+      <ScrollView
+        style={styles.tabsContainer}
+        onScroll={onScroll}
+        scrollEventThrottle={16}
+      >
+        {filteredPlans.map((plan, index) => (
+          <TouchableOpacity
+            onPress={() => togglePlanInfoPageVisibility(plan)}
+            key={index}
+          >
+            <PlanTabs
+              text={plan.name}
+              length={plan.length}
+              data={plan.data}
+              imageSource={require("../../../assets/accountbgdark.jpg")}
+            />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+      {PlanInfoPageVisibility && (
+        <PlanInfoPage
+          onClose={() => setPlanInfoPageVisibility(false)}
+          plan={planInfo}
+          windowWidth={windowWidth}
+          windowHeight={windowHeight}
+          image={require("../../../assets/accountbgdark.jpg")}
+        />
+      )}
+    </View>
+  );
 }
 
-const windowWidth = Dimensions.get("window").width; //IDK why but if u want symetry, you have to offset 20 px
-const windowHeight = Dimensions.get("window").height;
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  closeButton: {
-    position: "absolute",
-    top: 20,
-    right: 20,
-    zIndex: 1,
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  closeButtonText: {
-    color: "white",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: windowWidth,
-    height: windowHeight,
-    backgroundColor: "white",
-  },
-  tabsContainer: {
-    width: "100%",
-    top: 80,
-  },
-  searchBarContainer: {
-    position: "absolute",
-    top: 20,
-    left: 80,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  searchBar: {
-    width: 200,
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    borderRadius: 5,
-    paddingLeft: 10,
-    color: "#000",
-  },
+ container: {
+   flex: 1,
+   justifyContent: "center",
+   alignItems: "center",
+ },
+ overlay: {
+   position: "absolute",
+   top: 0,
+   left: 0,
+   width: windowWidth,
+   height: windowHeight,
+   backgroundColor: "white",
+ },
+ tabsContainer: {
+   width: "100%",
+   marginTop: 60, // Adjust the top margin
+ },
+ searchBarContainer: {
+   position: "absolute",
+   top: 20,
+   left: 80,
+   flexDirection: "row",
+   alignItems: "center",
+ },
+ top20: {
+   top: 10,
+ },
+ top80: {
+   top: 80,
+ },
+ searchBar: {
+   width: 200,
+   height: 40,
+   borderColor: "gray",
+   borderWidth: 1,
+   borderRadius: 5,
+   paddingLeft: 10,
+   color: "#000",
+ },
+ searchBarHidden: {
+   opacity: 0,
+ },
 });
--- src/components/Plans/PlanInfoPage.js
+++ src/components/Plans/PlanInfoPage.js
@@ -5,49 +5,53 @@ import {
   Dimensions,
   TouchableOpacity,
   Text,
+  Image,
+  ImageBackground,
 } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function PlanInfoPage({
   onClose,
   windowWidth,
   windowHeight,
   plan,
+  image,
 }) {
-  const lineLength = Dimensions.get("window").height - 265;
-
   return (
-    <View
-      style={[styles.container, { width: windowWidth, height: windowHeight }]}
-    >
-      <View style={styles.overlay} />
-      <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-        <Text style={styles.closeButtonText}>X</Text>
-      </TouchableOpacity>
-      <Text style={styles.planName}>{plan.name}</Text>
-      <Text style={styles.planLength}>{plan.length} days</Text>
-      <View style={styles.centeredBox}>
-        <Text>{plan.data}</Text>
-      </View>
-      <TouchableOpacity style={styles.addPlanButton}>
-        <Text style={styles.addPlanButtonText}>Add Plan</Text>
-      </TouchableOpacity>
+    <View style={styles.container}>
+      <ImageBackground
+        source={image} // Replace with the actual path to your background image
+        style={styles.overlay}
+        blurRadius={5} // Adjust the blur intensity as needed
+      >
+        <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+          <Text style={styles.closeButtonText}>X</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bookmarkButton}>
+          <FontAwesome5 name="bookmark" color="orange" />
+        </TouchableOpacity>
+        <Text style={styles.planName}>{plan.name}</Text>
+        <Text style={styles.planLength}>{plan.length} days</Text>
+        <View style={styles.centeredBox}>
+          <Text>{plan.data}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
 
-const windowWidth = Dimensions.get("window").width - 20; // Offset 20 px for symmetry
-const windowHeight = Dimensions.get("window").height;
+
+const windowWidth = Dimensions.get("window").width; // Offset 20 px for symmetry
+const windowHeight = Dimensions.get("window").height -60;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+   width: windowWidth, height: windowHeight-40
   },
   closeButton: {
     position: "absolute",
     top: 20,
-    right: 20,
+    right: 10,
     zIndex: 1,
     width: 40,
     height: 40,
@@ -61,54 +65,58 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
-  overlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: windowWidth,
-    height: windowHeight,
-    backgroundColor: "white",
-  },
-  addPlanButton: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: windowWidth,
-    borderRadius: 7,
-    height: 50,
-    backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  addPlanButtonText: {
-    color: "white",
-    fontSize: 16,
-  },
-  centeredBox: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: [{ translateX: -150 }, { translateY: -170 }],
-    width: 300,
-    height: 350,
-    borderRadius: 20,
-    backgroundColor: "gray",
-    justifyContent: "top",
-    padding: 20,
-  },
-  planLength: {
-    position: "absolute",
-    top: "6%",
-    fontWeight: "bold",
-    fontSize: 15,
-    color: "#646464",
-    left: 40,
-  },
-  planName: {
-    position: "absolute",
-    top: "5%",
-    fontWeight: "bold",
-    fontSize: 20,
-    textAlign: "center",
-  },
+  bookmarkButtonText: {
+   color: "orange",
+   fontSize: 18,
+   fontWeight: "bold",
+ },
+ bookmarkButton: {
+  position: "absolute",
+  top: 20,
+  right: 70,
+  zIndex: 1,
+  width: 40,
+  height: 40,
+  borderRadius: 20,
+  backgroundColor: "rgba(0, 0, 0, 0.2)",
+  justifyContent: "center",
+  alignItems: "center",
+ },
+ overlay: {
+   position: "absolute",
+   top: 0,
+   left: 0,
+   width: windowWidth,
+   height: windowHeight,
+   backgroundColor: "white",
+ },
+ centeredBox: {
+   position: "absolute",
+   top: "50%",
+   left: "50%",
+   transform: [{ translateX: -150 }, { translateY: -170 }],
+   width: 300,
+   height: 350,
+   borderRadius: 20,
+   backgroundColor: "gray",
+   justifyContent: "top",
+   padding: 20,
+ },
+ planLength: {
+   position: "absolute",
+   top: "6%",
+   fontWeight: "bold",
+   fontSize: 15,
+   color: "#646464",
+   left: 40,
+ },
+ planName: {
+  position: "absolute",
+  top: "5%",
+  left: 0,
+  right: 0,
+  fontWeight: "bold",
+  fontSize: 20,
+  textAlign: "center",
+},
 });
