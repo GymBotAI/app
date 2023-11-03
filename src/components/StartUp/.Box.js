853ed0d47375f36d90296cd67b3c4ac9d6863b87
ShiGame45 <shiven.sheth@gmail.com>
2023-11-03 17:46:34 +0100

    fixed

--- src/components/StartUp/.Box.js
+++ src/components/StartUp/.Box.js
@@ -136,7 +136,7 @@ export default function StartUpBox ({ navigation }) {
           borderRadius: 2,
         }}
         onPress={() => {
-          navigation.navigate("SignUp");
+          navigation.navigate("Home");
         }}
       >
         <Text>Skip to Home Screen (for devs)</Text>
