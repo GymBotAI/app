5ddacec7cfc64f60fd011196333f258ca3f7f8dd
ShiGame45 <shiven.sheth@gmail.com>
2023-11-04 17:13:56 +0100

    removed unncessary import

--- src/screens/Workout/CompleteWorkout/ExerciseSelection.tsx
+++ src/screens/Workout/CompleteWorkout/ExerciseSelection.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { View, Text, FlatList, ScrollView, TouchableOpacity } from "react-native";
 
 import { FontAwesome } from "@expo/vector-icons"; // You may need to import FontAwesome or an appropriate icon library
-
-import { upperExercises, lowerExercises, bothExercises } from "./ExerciseList";
 
 export default function ExerciseSelectionScreen({
   setSelectedExercises,
