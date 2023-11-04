fe137e61350687431d8616b555a3aa523e5f4676
lafkpages <luisafk@proton.me>
2023-11-04 12:40:17 +0100

    StartUp screen go to home if already logged in (tmp)

--- src/screens/StartUp.js
+++ src/screens/StartUp.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
 import { View, StatusBar } from "react-native";
+
+import { AppContext } from "../context/AppContext";
 
 import StartupScreen from "../components/StartUp/.Box";
 
 export default function StartUp({ navigation }) {
+  const { session } = useContext(AppContext);
+
+  if (session?.user) {
+    return navigation.navigate("Home");
+
+    // TODO: there should be a better way to do this,
+    // like <NavigationContainer> initialState.
+    // This is a temporary workaround.
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <StartupScreen navigation={navigation} />
