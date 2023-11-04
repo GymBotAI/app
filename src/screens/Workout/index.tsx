46fcdbf221e873afcebb5e0d7e83bc6f811e802f
ShiGame45 <shiven.sheth@gmail.com>
2023-11-04 15:45:43 +0100

    another nested navigation stack

--- src/screens/Workout/index.tsx
+++ src/screens/Workout/index.tsx
@@ -2,18 +2,29 @@ import { View, StatusBar } from "react-native";
 
 import WorkoutScreen from "../../components/Workout/.Box";
 import ScreenHeader from "../../components/ScreenHeader";
+import DesignWorkout from "../../components/Workout/DesignWorkout/.Box";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import type { NavigationScreens } from "../../types/navigation";
+
+const Stack = createNativeStackNavigator<NavigationScreens>();
 
 export default function Workouts({ navigation }) {
   return (
-    <View
-      style={{
-        height: "100%",
-        width: "100%",
-      }}
-    >
-      <ScreenHeader title="Workouts" />
+    <View style={{height: "100%", width: "100%"}}>
+        <ScreenHeader title="Workouts" />
 
-      <WorkoutScreen navigation={navigation} />
+        <Stack.Navigator
+        screenOptions={{
+        animation: 'none',
+        headerShown: false, // Hide the default header
+        }}
+        >
+
+          <Stack.Screen name="Workout" component={WorkoutScreen}/>
+          <Stack.Screen name="DesignWorkout" component={DesignWorkout}/>
+
+        </Stack.Navigator>
 
       <StatusBar barStyle="dark-content" />
 
