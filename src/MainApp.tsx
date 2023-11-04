c6a7533b1cc025218dc24df7175ef84f3c41b520
lafkpages <luisafk@proton.me>
2023-11-04 20:13:49 +0100

    Prettify MainApp

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,70 +1,70 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-//Screen Icons
+// Icons
 import { FontAwesome5 } from "@expo/vector-icons";
 
-//Tab Screens
+// Tab screens
 import Home from "./screens/Home";
 import Plans from "./screens/Plans";
 import Workouts from "./screens/Workout";
 import Food from "./screens/Food";
 import Chat from "./screens/Chat";
 
-const Tab = createBottomTabNavigator();
+const Tabs = createBottomTabNavigator();
 
 export default function MainApp() {
   return (
-    <Tab.Navigator
+    <Tabs.Navigator
       initialRouteName="Home"
       screenOptions={{
         headerShown: false, // Hide the default header
       }}
     >
-      <Tab.Screen
+      <Tabs.Screen
         name="Home"
         component={Home}
         options={{
           tabBarIcon: ({ color }) => (
-            <FontAwesome5 name={"home"} size={25} color={color} />
+            <FontAwesome5 name="home" size={25} color={color} />
           ),
         }}
       />
-      <Tab.Screen
+      <Tabs.Screen
         name="Plans"
         component={Plans}
         options={{
           tabBarIcon: ({ color }) => (
-            <FontAwesome5 name={"clipboard"} size={25} color={color} />
+            <FontAwesome5 name="clipboard" size={25} color={color} />
           ),
         }}
       />
-      <Tab.Screen
+      <Tabs.Screen
         name="Workouts"
         component={Workouts}
         options={{
           tabBarIcon: ({ color }) => (
-            <FontAwesome5 name={"dumbbell"} size={25} color={color} />
+            <FontAwesome5 name="dumbbell" size={25} color={color} />
           ),
         }}
       />
-      <Tab.Screen
+      <Tabs.Screen
         name="Food"
         component={Food}
         options={{
           tabBarIcon: ({ color }) => (
-            <FontAwesome5 name={"apple-alt"} size={25} color={color} />
+            <FontAwesome5 name="apple-alt" size={25} color={color} />
           ),
         }}
       />
-      <Tab.Screen
+      <Tabs.Screen
         name="Chat"
         component={Chat}
         options={{
           tabBarIcon: ({ color }) => (
-            <FontAwesome5 name={"comment"} size={25} color={color} />
+            <FontAwesome5 name="comment" size={25} color={color} />
           ),
         }}
       />
-    </Tab.Navigator>
+    </Tabs.Navigator>
   );
 }
