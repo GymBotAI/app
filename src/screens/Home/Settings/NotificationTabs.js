65c11fbe3fe3b56754f6adebf9de634938470c57
lafkpages <luisafk@proton.me>
2023-11-09 15:15:53 +0100

    Use path aliases in imports: src/screens/Home/Settings/NotificationTabs.js

--- src/screens/Home/Settings/NotificationTabs.js
+++ src/screens/Home/Settings/NotificationTabs.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 
-import { colors } from "../../../components/styles";
+import { colors } from "$styles";
 
 export default function NotificationsTab({ title, description }) {
   return (
