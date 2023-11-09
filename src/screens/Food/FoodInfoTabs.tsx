bb9b77a6c29637fb82b2014ca0a78882f6142f8f
lafkpages <luisafk@proton.me>
2023-11-09 15:31:10 +0100

    Prettify: src/screens/Food/FoodInfoTabs.tsx

--- src/screens/Food/FoodInfoTabs.tsx
+++ src/screens/Food/FoodInfoTabs.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import { colors } from "$styles";
 
