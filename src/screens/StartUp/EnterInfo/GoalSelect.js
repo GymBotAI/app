57033fecf1ee1c2e67f97b9d9080fc36e101b557
lafkpages <luisafk@proton.me>
2023-11-09 15:31:11 +0100

    Prettify: src/screens/StartUp/EnterInfo/GoalSelect.js

--- src/screens/StartUp/EnterInfo/GoalSelect.js
+++ src/screens/StartUp/EnterInfo/GoalSelect.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 export let Goals = [];
 
