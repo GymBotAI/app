24a5bd33163b9ce49269bf059d79c2d9e38c7dca
lafkpages <luisafk@proton.me>
2023-11-09 15:15:52 +0100

    Use path aliases in imports: src/context/AppContext.ts

--- src/context/AppContext.ts
+++ src/context/AppContext.ts
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-import type { Database } from "../types/database";
+import type { Database } from "$types/database";
 import type { Session } from "@supabase/supabase-js";
 
 export type UserData = Partial<Database["public"]["Tables"]["users"]["Row"]>;
