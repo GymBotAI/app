a49a63ed324d56204c3cee70ec9115fadf19dec8
lafkpages <luisafk@proton.me>
2023-11-09 15:31:10 +0100

    Prettify: src/screens/Chat/components/ChatPrompts.js

--- src/screens/Chat/components/ChatPrompts.js
+++ src/screens/Chat/components/ChatPrompts.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 import { colors } from "$styles";
 
