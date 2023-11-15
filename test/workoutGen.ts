fb3710a77456ce6e5c35c91c165f5ffc6f6efdf4
lafkpages <luisafk@proton.me>
2023-11-15 16:31:38 +0100

    Update workout generator example

--- test/workoutGen.ts
+++ test/workoutGen.ts
@@ -5,7 +5,9 @@ import { generateWorkout } from "$api/workouts";
 // Generate an example workout and log it to the console
 
 generateWorkout({
-  duration: 10,
-  bodypart: "chest",
-  notes: null,
+  duration: 20,
+  equipment: ["dumbbells", "running machine"],
+  goal: "Gain more muscle",
+  subgoal: "Arm strength",
+  notes: "Slight discomfort in chest, happening for three days",
 }).then(console.log);
