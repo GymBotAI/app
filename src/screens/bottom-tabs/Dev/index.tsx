10505a0238a9ee2e5e3f3c645103d6497a0c0e1a
lafkpages <luisafk@proton.me>
2023-12-13 15:47:32 +0100

    Dev screen home

--- src/screens/bottom-tabs/Dev/index.tsx
+++ src/screens/bottom-tabs/Dev/index.tsx
@@ -1,13 +1,29 @@
+import { Text, View } from "react-native";
+
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Button from "./screens/Button";
 
 const Drawer = createDrawerNavigator();
 
-export default function Workouts() {
+export default function Dev() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Button" component={Button} />
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Group>
+        <Drawer.Screen name="Button" component={Button} />
+      </Drawer.Group>
     </Drawer.Navigator>
+  );
+}
+
+function Home() {
+  return (
+    <View
+      style={{ display: "flex", flexDirection: "column", gap: 12, padding: 12 }}
+    >
+      <Text>Dev docs</Text>
+      <Text>This screen is only shown in the bottom navbar in dev mode.</Text>
+    </View>
   );
 }
