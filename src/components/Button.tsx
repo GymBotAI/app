3b1e7fac7f471f22474d2de65252f51747eb85b0
lafkpages <luisafk@proton.me>
2023-12-13 15:13:46 +0100

    Finish basic Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,76 @@
 import type { TouchableOpacityProps } from "react-native";
 
-import { TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 import { colors } from "$styles";
 
-export default function Button(props: TouchableOpacityProps) {
+export type ButtonProps = TouchableOpacityProps & {
+  text: string;
+  size?: "small" | "medium" | "large";
+};
+
+function buttonStyles(props: ButtonProps) {
+  const baseStyles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.blue.default,
+    borderRadius: 28,
+    shadowColor: colors.black.default,
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  } as const;
+
+  const sizeStyles = StyleSheet.create({
+    large: {
+      padding: 10,
+    },
+    medium: {
+      padding: 8,
+    },
+    small: {
+      padding: 6,
+    },
+  });
+
+  return {
+    ...baseStyles,
+    ...sizeStyles[props.size],
+  };
+}
+
+function textStyles(props: ButtonProps) {
+  const baseStyles = {
+    color: "#ededed",
+    fontWeight: "bold",
+  } as const;
+
+  const sizeStyles = StyleSheet.create({
+    large: {
+      fontSize: 28,
+    },
+    medium: {
+      fontSize: 26,
+    },
+    small: {
+      fontSize: 24,
+    },
+  });
+
+  return {
+    ...baseStyles,
+    ...sizeStyles[props.size],
+  };
+}
+
+export default function Button(props: ButtonProps) {
   return (
     <TouchableOpacity
       {...props}
-      style={{
-        position: "absolute",
-        bottom: 40,
-        alignSelf: "center",
-        width: "75%",
-        backgroundColor: colors.blue.default,
-        borderRadius: 28,
-        shadowColor: colors.black.default,
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.2,
-        shadowRadius: 2,
-      }}
+      style={StyleSheet.compose(buttonStyles(props), props.style)}
     >
-      {props.children}
+      <Text style={textStyles(props)}>{props.text}</Text>
     </TouchableOpacity>
   );
 }
