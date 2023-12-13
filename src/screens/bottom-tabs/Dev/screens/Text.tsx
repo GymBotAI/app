eabd4fcff0b06f2edff2c0de7429131694dd4aed
lafkpages <luisafk@proton.me>
2023-12-13 16:24:20 +0100

    Text component docs code examples

--- src/screens/bottom-tabs/Dev/screens/Text.tsx
+++ src/screens/bottom-tabs/Dev/screens/Text.tsx
@@ -10,6 +10,14 @@ export default function TextDevScreen() {
       <Text text="Text" variant="header-big" />
       <Text text="The text component is used for showing text." />
 
+      <Text text="Examples" variant="header-default" />
+      <Text text="First, import it from `$components/Text`." />
+      <Text
+        text={`<Text text="This is a normal text" />
+<Text text="This is a bold text" bold />
+<Text text="This is a big header" variant="header-big" />`}
+      />
+
       <Text text="Variants" variant="header-default" />
       <Text text="All the existing text variants are listed below:" />
 
