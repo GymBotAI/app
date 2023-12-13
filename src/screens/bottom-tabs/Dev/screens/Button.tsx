249f40032a3ca5db232b144e0a105a4ba4173de8
lafkpages <luisafk@proton.me>
2023-12-13 16:39:21 +0100

    Way better Button component docs

--- src/screens/bottom-tabs/Dev/screens/Button.tsx
+++ src/screens/bottom-tabs/Dev/screens/Button.tsx
@@ -1,7 +1,9 @@
-import { useCallback } from "react";
-import { Alert, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 
 import Button from "$components/Button";
+import Text from "$components/Text";
+
+import SyntaxHighlighter from "react-native-syntax-highlighter";
 
 export default function ButtonDevScreen() {
   const onPress = () => {
@@ -12,9 +14,46 @@ export default function ButtonDevScreen() {
     <View
       style={{ display: "flex", flexDirection: "column", gap: 12, padding: 12 }}
     >
-      <Button text="Large button" size="large" onPress={onPress} />
-      <Button text="Medium button" size="medium" onPress={onPress} />
-      <Button text="Small button" size="small" onPress={onPress} />
+      <Text text="Button" variant="header-big" />
+      <Text text="Renders a styled button." />
+
+      <Text text="Examples" variant="header-default" />
+
+      <SyntaxHighlighter>
+        {'<Button text="Large button" size="large" onPress={onPress} />'}
+      </SyntaxHighlighter>
+      <Button
+        text="Large button"
+        size="large"
+        onPress={onPress}
+        style={styles.exampleButtons}
+      />
+
+      <SyntaxHighlighter>
+        {'<Button text="Medium button" size="medium" onPress={onPress} />'}
+      </SyntaxHighlighter>
+      <Button
+        text="Medium button"
+        size="medium"
+        onPress={onPress}
+        style={styles.exampleButtons}
+      />
+
+      <SyntaxHighlighter>
+        {'<Button text="Small button" size="small" onPress={onPress} />'}
+      </SyntaxHighlighter>
+      <Button
+        text="Small button"
+        size="small"
+        onPress={onPress}
+        style={styles.exampleButtons}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  exampleButtons: {
+    marginBottom: 12,
+  },
+});
