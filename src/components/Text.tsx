a800b82f936b9d166d2497276bab90f8ed1850e1
lafkpages <luisafk@proton.me>
2023-12-13 16:48:59 +0100

    Text component prop types omit children

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,15 +1,15 @@
 import type { StylesObject } from "$types/styles";
-import { size } from "mathjs";
 import type { TextProps as RNTextProps } from "react-native";
-import { Dimensions } from "react-native";
 
-import { Text as RNText, StyleSheet } from "react-native";
+import { Dimensions, Text as RNText, StyleSheet } from "react-native";
+
+import { size } from "mathjs";
+
 const { width, height } = Dimensions.get("window");
 const holder = Math.min(width, height);
 const mediumFont = holder * 0.055;
 const largeFont = holder * 0.095;
 console.log(largeFont);
-
 
 /**
  * The variant prop is used to apply a predefined set of styles to the text.
@@ -29,7 +29,7 @@ export interface TextStylingProps {
 export interface TextVariantProps {
   variant: TextVariant;
 }
-export type TextProps = RNTextProps & {
+export type TextProps = Omit<RNTextProps, "children"> & {
   text: string;
 } & (TextStylingProps | TextVariantProps);
 
