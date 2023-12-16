1817b20cff6f5dc83174fbe23e5cd6dc65dfa3b6
lafkpages <luisafk@proton.me>
2023-12-16 19:21:24 +0100

    Types for importing static assets

---
+++ src/types/assets.d.ts
@@ -0,0 +1,14 @@
+declare module "*.webp" {
+  const asset: number;
+  export default asset;
+}
+
+declare module "*.png" {
+  const asset: number;
+  export default asset;
+}
+
+declare module "*.ttf" {
+  const asset: number;
+  export default asset;
+}
