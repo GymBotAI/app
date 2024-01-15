0549f52921bf6f5eb4562778b97f7c6616ad10b6
sodiaco007 <sodenlucas@gmail.com>
2024-01-15 17:28:31 +0100

    colors fixed

--- styles.ts
+++ styles.ts
@@ -11,16 +11,12 @@ export interface ColorVariants {
 
 export const colors = {
   primary: { default: "#1877F2", lighter: "#4392f9", lightest: "#8ED9DE" },
-  positive: {default: "#808080", lighter: "#D8D8D8", lightest: "#F5F5F5", darker: "#a9a9a9"},
+  positive: {default: "#efefef", lighter: "#f5f5f5", lightest: "#ffffff", darker: "#a9a9a9"},
+  negative: {default: '#FF0000'},
   black: { default: "#000000", lighter: "#333333", lightest: "#444444" },
   blue: { default: "#1877F2", lighter: "#4392f9", lightest: "#8ED9DE" },
   green: { default: "#00DD33", lightest: "#8EDE99" },
-  grey: {
-    default: "#808080",
-    lighter: "#D8D8D8",
-    lightest: "#F5F5F5",
-    darker: "#a9a9a9",
-  },
+  grey: { default: "#808080", lighter: "#D8D8D8", lightest: "#F5F5F5", darker: "#a9a9a9"},
   orange: { default: "#FFA500" },
   red: { default: "#FF0000" },
   white: { default: "#FFFFFF" },
